74883818ec1ad933d17a3ec860160d389d609a53
Nils Jung <nillingertrash@gmail.com>
2024-11-18 14:58:17 +0100

    z_range

--- pyransac3d/cylinder.py
+++ pyransac3d/cylinder.py
@@ -25,13 +25,14 @@ class Cylinder:
         self.axis = []
         self.radius = 0
 
-    def fit(self, pts, thresh=0.2, maxIteration=10000):
+    def fit(self, pts, thresh=0.2, maxIteration=10000, z_range=1):
         """
         Find the parameters (axis and radius) defining a cylinder.
 
         :param pts: 3D point cloud as a numpy array (N,3).
         :param thresh: Threshold distance from the cylinder hull which is considered inlier.
         :param maxIteration: Number of maximum iteration which RANSAC will loop over.
+        :param z_range: Maximum vertical distance between the three sampled points.
 
         :returns:
         - `center`: Center of the cylinder np.array(1,3) which the cylinder axis is passing through.
@@ -46,9 +47,34 @@ class Cylinder:
 
         for it in range(maxIteration):
 
-            # Samples 3 random points
-            id_samples = random.sample(range(0, n_points), 3)
-            pt_samples = pts[id_samples]
+            while True:
+                # Sample the first random point
+                first_id_sample = random.sample(range(0, n_points), 1)[0]
+                first_pt_sample = pts[first_id_sample]
+                # print(first_pt_sample)
+
+                # Establish z-range
+                z_center = first_pt_sample[2]
+                z_min = z_center - (z_range / 2)
+                z_max = z_center + (z_range / 2)
+
+                # Reduce point Cloud to z-Range
+                remaining_pts = pts[np.arange(n_points) != first_id_sample]
+                filtered_pts = remaining_pts[(remaining_pts[:, 2] >= z_min) & (remaining_pts[:, 2] <= z_max)]
+
+                # print(filtered_pts)
+
+                # Check if we have enough points to sample from
+                if len(filtered_pts) >= 2:
+                    break
+                else:
+                    print("Not enough points within the specified z-axis range. Choosing a new first point.")
+
+            # Sample the remaining 2 points from the reduced cloud
+            rem_id_samples = random.sample(range(len(filtered_pts)), 2)
+            rem_pt_samples = filtered_pts[rem_id_samples]
+
+            pt_samples = np.vstack((first_pt_sample, rem_pt_samples))
 
             # We have to find the plane equation described by those 3 points
             # We find first 2 vectors that are part of this plane
@@ -83,12 +109,12 @@ class Cylinder:
 
             # Calulate the center by verifying intersection of each orthogonal line
             p_center_x = (
-                ma * mb * (P_rot[0, 1] - P_rot[2, 1])
-                + mb * (P_rot[0, 0] + P_rot[1, 0])
-                - ma * (P_rot[1, 0] + P_rot[2, 0])
-            ) / (2 * (mb - ma))
+                                 ma * mb * (P_rot[0, 1] - P_rot[2, 1])
+                                 + mb * (P_rot[0, 0] + P_rot[1, 0])
+                                 - ma * (P_rot[1, 0] + P_rot[2, 0])
+                         ) / (2 * (mb - ma))
             p_center_y = -1 / (ma) * (p_center_x - (P_rot[0, 0] + P_rot[1, 0]) / 2) + (P_rot[0, 1] + P_rot[1, 1]) / 2
-            p_center = [p_center_x, p_center_y, P_rot[0,2]]
+            p_center = [p_center_x, p_center_y, P_rot[0, 2]]
             radius = np.linalg.norm(p_center - P_rot[0, :])
 
             # Remake rodrigues rotation
